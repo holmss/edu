23f55d7a10acd7f7f438c0d2e03db4224496697b
holmss <44115172+holmss@users.noreply.github.com>
2018-11-07 16:29:12 +0300

    frst tw smpls

---
+++ courses/brackets1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Brackets
+    {
+        public static bool Check()
+        {
+
+            int square = 0, round = 0;
+            bool ind = false;
+            Console.Write(">> ");
+            string s = Console.ReadLine();
+
+            //! null
+            foreach (char i in s)
+            {
+                NewMethod(ref square, ref round, ref ind, i);
+                if (round == 0)
+                    ind = false;
+                if (round < 0 || square < 0)
+                    break;
+                if (ind && square == 0 && round != 0)
+                    break;
+            }
+            return (round == 0 && square == 0 ? true : false);
+        }
+
+        private static void NewMethod(ref int square, ref int round, ref bool ind, char i)
+        {
+            switch (i)
+            {
+                case ')':
+                    round--; break;
+                case '(':
+                    round++; break;
+                case '[':
+                    square++; ind = true; break;
+                case ']':
+                    square--; ind = true; break;
+                default:
+                    break;
+            }
+        }
+
+        public static void Main()
+        {
+            bool res = Check();
+            Console.WriteLine(res);
+
+            //Console.ReadKey();
+        }
+    }
+}
---
+++ courses/brackets2.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Example
+{
+    public class Progaem
+    {
+        public static void Main()
+        {
+            Console.Write(">>");
+
+            string inp = Console.ReadLine();
+            bool result = false;
+            if (inp != null)
+            {
+                char[] myInp = inp.ToCharArray();
+                
+                List<int> resultArr = new List<int>();
+                bool forceStop = false;
+                for (int i = 0; i < myInp.Length; i++)
+                {
+                    char temp = myInp[i];
+
+
+                    if (temp == '(')
+                    {
+                        resultArr.Add(1);
+                        Console.Write(" +");
+                    }
+                    else if (temp == ')')
+                    {
+                        if (resultArr.Count > 0)
+                        {
+                            resultArr.RemoveAt(resultArr.Count - 1);
+                            Console.Write(" -");
+                        }
+                        else
+                        {
+                            forceStop = true;
+                            break;
+                        }
+                    }
+                }
+                if (resultArr.Count == 0 && !forceStop)
+                {
+                    result = true;
+                }
+            }
+            
+            Console.WriteLine();
+            Console.WriteLine(result.ToString());
+
+            //Console.ReadKey();
+        }
+
+        //public static void Main()
+        //{
+        //    Console.Write(">>");
+        //    char[] s = (Console.ReadLine()).ToCharArray();
+        //    if (s.Length != 0)
+        //    {
+        //        bool ind = true, closer = true;
+
+        //        List<int> square = new List<int>();
+        //        List<int> round = new List<int>();
+
+        //        for (int i = 0; i < s.Length; i++)
+        //        {
+        //            char temp = s[i];
+
+        //            if (temp == '(')
+        //            {
+
+        //                round.Add(1);
+        //                Console.Write(" +");
+        //            }
+        //            else if (temp == ')')
+        //            {
+        //                if (round.Count > 0)
+        //                {
+
+        //                    round.RemoveAt(round.Count - 1);
+        //                    Console.Write(" -");
+        //                }
+        //                else
+        //                {
+        //                    closer = false;
+        //                    break;
+        //                }
+        //            }
+
+        //            if (temp == '[')
+        //            {
+
+        //                square.Add(1);
+        //                Console.Write(" +");
+        //            }
+        //            else if (temp == ']')
+        //            {
+        //                if (square.Count > 0)
+        //                {
+
+        //                    square.RemoveAt(square.Count - 1);
+        //                    Console.Write(" -");
+        //                }
+        //                else
+        //                {
+        //                    closer = false;
+        //                    break;
+        //                }
+        //            }
+        //        }
+        //        if (round.Count == 0 && square.Count == 0 && !ind && closer)
+        //        {
+        //            closer = true;
+        //        }
+        //        Console.WriteLine(" " + closer);
+        //    }
+        //    //Console.ReadKey();
+        //}
+    }
+}
