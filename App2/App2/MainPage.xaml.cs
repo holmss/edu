e96b52dab0160d9d389d09618829fb38f33be4af
holmss <vlrlskv@gmail.com>
2018-12-08 12:04:21 +0300

    cmt
    c

---
+++ App2/App2/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
+
+namespace App2
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            BackButton.Visibility = Visibility.Collapsed;
+            MyPage.IsSelected = true;
+            MyFrame.Navigate(typeof(MyPage));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(MyPage.IsSelected)
+            {
+                MyFrame.Navigate(typeof(MyPage));
+                BackButton.Visibility = Visibility.Collapsed;
+            }
+            if(News.IsSelected)
+            {
+                MyFrame.Navigate(typeof(NewsPage));
+                BackButton.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(MyFrame.CanGoBack)
+            {
+                MyFrame.GoBack();
+                MyPage.IsSelected = true;
+            }
+        }
+    }
+}
